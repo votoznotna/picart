0ca85bbe72917969c1e41745d5bbb50cbac33db3
Anton Zotov <votoznotna@gmail.com>
2015-01-28 13:13:31 -0800

    Sync with Git.

--- config/env/production.js
+++ config/env/production.js
@@ -10,15 +10,17 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
 			],
 			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.min.js',
+				'public/lib/angular-cookies/angular-cookies.min.js',
+				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-touch/angular-touch.min.js',
+				'public/lib/angular-sanitize/angular-sanitize.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/angularjs-imageupload-directive/public/javascripts/imageupload.js',
+				'public/lib/angular-grecaptcha/grecaptcha-min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
--- public/modules/galleries/controllers/galleries.client.controller.js
+++ public/modules/galleries/controllers/galleries.client.controller.js
@@ -113,19 +113,6 @@ angular.module('galleries').controller('GalleriesController', ['$scope', '$state
             $scope.gallery = angular.copy($scope.master);
         };
 
-        $scope.uploadPicture = function(image) {
-            var formData = new FormData();
-            formData.append('image', image, image.name);
-
-            $http.post('upload', formData, {
-                headers: { 'Content-Type': false },
-                transformRequest: angular.identity
-            }).success(function(result) {
-                $scope.uploadedImgSrc = result.src;
-                $scope.sizeInBytes = result.size;
-            });
-        };
-
         $scope.clearPicture = function() {
             $scope.gallery.picture  = null;
             angular.element(document.querySelector('#picture')).val("");
